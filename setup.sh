244d7604e82634fd313a638f227e92d6bee5022c
senkenn <senken32@gmail.com>
2022-11-27 21:28:25 +0900

    Update vscode

--- setup.sh
+++ setup.sh
@@ -9,16 +9,17 @@ git config --global user.name senkenn
 git config --global user.email senken32@gmail.com
 git config --global init.defaultBranch main
 
-# install docker, docker compose
+# Install Docker, Docker Compose 
 sudo snap install docker
 
 # capslock -> ctrl
 sudo sh -c "echo 'evdev:atkbd:dmi:bvn*:bvr*:bd*:svn*:pn*:pvr*\n\tKEYBOARD_KEY_3A=leftctrl' > /etc/udev/hwdb.d/90-nocaps-keyboard.hwdb" 
 
-# install vscode
-sudo apt install -y code
+# Install VSCode
+curl -L https://go.microsoft.com/fwlink/?LinkID=760868 -o vscode.deb
+sudo apt install -y ./vscode.deb && rm ./vscode.deb
 
-# instal chrome
+# Install Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install -y ./google-chrome-stable_current_amd64.deb
 
@@ -26,4 +27,4 @@ sudo apt install -y ./google-chrome-stable_current_amd64.deb
 sudo apt install -y discord
 
 # install copyq
-sudo apt install copyq
+sudo apt install -y copyq
