af9652e8acd1e240540cdd9b3192ad7680504cb7
senkenn <senken32@gmail.com>
2022-12-11 20:32:07 +0900

    Update keybindings

--- keybindings.sh
+++ keybindings.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Super>Page_Up', '<Super><Alt>Left', '<Control><Super>Left']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Super>Page_Down', '<Super><Alt>Right', '<Control><Super>Right']"
+gsettings set org.gnome.shell.keybindings show-screenshot-ui "['Print', '<Super><Shift>s']"
