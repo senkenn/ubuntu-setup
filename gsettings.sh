27da7699fe54fa3f054a521953fe56b4bce41107
senkenn <senken32@gmail.com>
2022-12-30 21:19:21 +0900

    Update keybindings

--- gsettings.sh
+++ gsettings.sh
@@ -10,6 +10,7 @@ gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['<Contro
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['<Control><Super>Down']"
 gsettings set org.gnome.shell.keybindings show-screenshot-ui "['Print', '<Super><Shift>s']"
 gsettings set org.gnome.shell.keybindings toggle-message-tray "['<Ctrl><Super>v', '<Super>m']"
+gsettings set org.freedesktop.ibus.panel.emoji hotkey "[]" # previous setting: ['<Control>period', '<Control>semicolon']
 
 # UI
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
