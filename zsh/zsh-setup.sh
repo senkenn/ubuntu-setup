6cab8d5e319b4de28b577f067f2d6d7e29090ad4
SENKEN <65453007+senkenn@users.noreply.github.com>
2023-01-17 00:02:12 +0900

    Update zsh-setup.sh

--- zsh/zsh-setup.sh
+++ zsh/zsh-setup.sh
@@ -4,8 +4,13 @@
 sudo apt-get install -y zsh
 sudo sed -i.bak "s|$HOME:/bin/bash|$HOME:/bin/zsh|" /etc/passwd
 
-# Powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.p10k
-
+# install zsh extension (prezto)
+git clone --recursive https://github.com/sorin-ionescu/prezto.git $HOME/.zprezto
+ln -s $HOME/.zprezto/runcoms/zlogin    $HOME/.zlogin \
+  && ln -s $HOME/.zprezto/runcoms/zlogout   $HOME/.zlogout \
+  && ln -s $HOME/.zprezto/runcoms/zpreztorc $HOME/.zpreztorc \
+  && ln -s $HOME/.zprezto/runcoms/zprofile  $HOME/.zprofile \
+  && ln -s $HOME/.zprezto/runcoms/zshenv    $HOME/.zshenv
+echo "zstyle ':prezto:module:prompt' theme 'powerlevel10k'" >> $HOME/.zpreztorc
 cp $HOME/ubuntu-setup/zsh/.zshrc $HOME
 cp $HOME/ubuntu-setup/zsh/.p10k.zsh $HOME
